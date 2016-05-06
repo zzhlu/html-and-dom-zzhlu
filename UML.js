2d77a02a632e98bfcfbf34c8db64955f8723b618
zhaolu <1632089314@qq.com>
2016-05-06 18:45:11 +0800

    save answer in array

--- UML.js
+++ UML.js
@@ -1,28 +1,60 @@
 window.onload = function () {
-    
-    var grade = document.getElementById("grade");
+    var grade = document.getElementById("computeGrade");
 
     grade.onclick = function () {
-        if (document.getElementById("stuClass").value !== "" && document.getElementById("stuId").value !== "" && document.getElementById("stuName").value !== "") {
-            document.getElementById("score").innerHTML = "得分：" + getScore(0);
+        if (getElementValueById("className") !== "" &&
+            getElementValueById("studentNumber") !== "" &&
+            getElementValueById("studentName") !== "") {
+
+            document.getElementById("score").innerHTML = "得分：" + computeTotalScore(0);
         } else {
             alert("请补全班级、学号、姓名等信息！");
         }
     };
 };
 
-function getElementValueById(id) {
+function computeTotalScore(score) {
+    var allAnswers = loadAllAnswers();
+    var firstQuestionIds = ["1-1", "1-2-1", "1-2-2", "1-2-3"];
+    var firstActualAnswers = buildFirstActualAnswers(firstQuestionIds);
+    var firstScore = computeFirstScore(firstActualAnswers, allAnswers, score);
 
+    var otherQuestionNames = ["2-1", "2-2", "3-1", "3-2", "4-1", "4-2"];
+    var OtherActualAnswers = buildOtherActualAnswers(otherQuestionNames);
+    var otherScore = computeOtherScore(OtherActualAnswers, allAnswers, score);
+
+    return firstScore + otherScore;
+}
+
+function getElementValueById(id) {
     return document.getElementById(id).value;
 }
 
-function getElementsByName(name) {
+function buildFirstActualAnswers(ids) {
+    var firstActualAnswers = [];
 
+    ids.forEach(function (id) {
+        firstActualAnswers.push(getElementValueById(id));
+    });
+
+    return firstActualAnswers;
+}
+
+function computeFirstScore(firstActualAnswers, allAnswers, score) {
+    for (var i = 0; i < firstActualAnswers.length; i++) {
+        if (allAnswers[0][i] === firstActualAnswers[i]) {
+            score += 5;
+        }
+    }
+
+    return score;
+}
+
+function getElementsByName(name) {
     return document.getElementsByName(name);
 }
 
 function getCheckedElementValue(elements) {
-
     var optionValue = '';
 
     for (var i = 0; i < elements.length; i++) {
@@ -34,41 +66,31 @@ function getCheckedElementValue(elements) {
     return optionValue;
 }
 
-function getScore(score) {
+function buildOtherActualAnswers(names) {
+    var otherActualAnswers = [];
 
-    if (getElementValueById("1-1") === "统一建模语言") {
-        score += 5;
-    }
-    if (getElementValueById("1-2-1") === "封装性") {
-        score += 5;
-    }
-    if (getElementValueById("1-2-2") === "继承性") {
-        score += 5;
-    }
-    if (getElementValueById("1-2-3") === "多态性") {
-        score += 5;
-    }
-    if (getCheckedElementValue(getElementsByName("2-1")) === "B") {
-        score += 10;
-    }
-    if (getCheckedElementValue(getElementsByName("2-2")) === "C") {
-        score += 10;
-    }
-    if (getCheckedElementValue(getElementsByName("3-1")) === "ABD") {
-        score += 10;
-    }
-    if (getCheckedElementValue(getElementsByName("3-2")) === "ABC") {
-        score += 10;
-    }
-    if (getCheckedElementValue(getElementsByName("4-1")) === "false") {
-        score += 10;
-    }
-    if (getCheckedElementValue(getElementsByName("4-2")) === "true") {
-        score += 10;
-    }
-    if (getElementValueById("5-1") === "模型是对现实世界的简化和抽象,模型是对所研究的系统、过程、事物或概念的一种表达形式。可以是物理实体;可以是某种图形;或者是一种数学表达式。") {
-        score += 20;
+    names.forEach(function (name) {
+        var elements = getElementsByName(name);
+        var optionValue = getCheckedElementValue(elements);
+        otherActualAnswers.push(optionValue);
+    });
+
+    return otherActualAnswers;
+}
+
+function computeOtherScore(otherActualAnswers, allAnswers, score) {
+    for (var i = 0; i < otherActualAnswers.length; i++) {
+        if (otherActualAnswers[i] === allAnswers[1][i]) {
+            score += 10;
+        }
     }
 
     return score;
+}
+
+function loadAllAnswers() {
+    return [
+        ["统一建模语言", "封装性", "继承性", "多态性"],
+        ["B", "C", "ABD", "ABC", "false", "true"]
+    ];
 }
